64da4568f478c314451f8fa1465224d841b901ac
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-31 19:46:33 +0700

    Working on the ShoppingListComponent

--- 01-the-basics/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 01-the-basics/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+	selector: "app-shopping-edit",
+	templateUrl: "./shopping-edit.component.html",
+	styleUrls: ["./shopping-edit.component.scss"],
 })
 export class ShoppingEditComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- 01-the-basics/src/app/shopping-list/shopping-list.component.ts
+++ 01-the-basics/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+	selector: "app-shopping-list",
+	templateUrl: "./shopping-list.component.html",
+	styleUrls: ["./shopping-list.component.scss"],
 })
 export class ShoppingListComponent implements OnInit {
+	ingredients = [];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
