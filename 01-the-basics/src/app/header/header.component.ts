530e994cde29fb67882771646ca4766e6f9b479e
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-31 17:54:42 +0700

    Adding a Navigation Bar

--- 01-the-basics/src/app/header/header.component.ts
+++ 01-the-basics/src/app/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
 	selector: "app-header",
 	templateUrl: "./header.component.html",
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+	collapsed = true;
+
+	constructor() {}
+
+	ngOnInit(): void {}
+}
